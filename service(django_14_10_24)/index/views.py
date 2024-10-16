2fa268e52c01beeef9d91ead984a2532bd14c261
Andrei Gumenniy <an.gumenniy@gmail.com>
2024-10-16 14:57:36 +0300

    Update views.py
    added quick search via tag

--- service(django_14_10_24)/index/views.py
+++ service(django_14_10_24)/index/views.py
@@ -1,17 +1,15 @@
 from django.core.paginator import Paginator
 from django.db.models import Sum, Count
 from django.db.models.functions import TruncMonth
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from info.models import Info
 from accident.models import Accident
 from .models import Index
+from fttx.models import Fttx
+from key.models import Key
+from manual.models import Manual
 from datetime import date
-
-
-
-
-# Create your views here.
 from django.views.generic import (
     ListView,
     DetailView,
@@ -29,9 +27,9 @@ class IndexDetailView(DetailView):
 class InfoDataView(ListView):
     model = Info
 
-now = date.today()
 
 def start_page(request):
+    now = date.today()
     start_date = timezone.datetime(now.year, 1, 1)
     end_date = timezone.datetime(now.year, 12, 31)
     ind = Index.objects.order_by("-date_created")[:100]  # 100 последних записей
@@ -64,3 +62,37 @@ def start_page(request):
     return render(request, "index/index_list.html", {"page_obj": page_obj, "data_list": data,
                                                      "date": dat, "accident": accident, "check": check})
 
+def search(request):
+    result = request.GET.get('q')
+    query = result.split(", ")
+    print("query", len(query))
+    if len(query) < 2 or len(query) > 30 :
+        return render(request, 'index/search_results.html', {'uncorrect': "Некорректные условия запроса! "})
+    if query[0] == "старт":
+        results = Index.objects.all()
+        if query[1]:
+            results = results.filter(content__icontains=query[1])
+            return render(request, 'index/search_results.html', {'uncorrect': results})
+    if query[0] == "инфо":
+        results = Info.objects.all()
+        if query[1]:
+            info_list = results.filter(name__icontains=query[1])
+            return render(request, 'info/info_list.html', {'info_list': info_list})
+    if query[0] == "инцидент":
+        results = Accident.objects.all()
+        if query[1]:
+            accident_name = results.filter(name__icontains=query[1]).order_by("id")
+            return render(request, 'accident/accident_list.html', {'accident_list': accident_name})
+    if query[0] == "мануал":
+        results = Manual.objects.all()
+        if query[1]:
+            manual = results.filter(type=query[1])
+            return render(request, 'manual/manual_list.html', {'manual_list': manual})
+    if query[0] == "fttx":
+        print(query[0], query[1], query[2], query[3])
+        fttx_search = get_object_or_404(Fttx, city=query[1], street=query[2], number=query[3])
+        return render(request, 'fttx/fttx_detail.html', {'fttx': fttx_search})
+    if query[0] == "ключ":
+        print(query[0], query[1], query[2], query[3])
+        key_search = get_object_or_404(Key, city=query[1], street=query[2], home=query[3])
+        return render(request, 'key/key_detail.html', {'key': key_search})
