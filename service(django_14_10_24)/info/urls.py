99770f929067cbf7bd9e4a3ebabd938ebb2808cd
Andrei Gumenniy <an.gumenniy@gmail.com>
2024-10-16 15:05:02 +0300

    Update urls.py
    added path urls

--- service(django_14_10_24)/info/urls.py
+++ service(django_14_10_24)/info/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
         views.InfoDetailView.as_view(),
         name="info-detail"
     ),
+    path('csv/actual', views.download_csv_actual_month, name='download_actual_csv'),
+    path('csv/', views.date_range_view, name='date_range'),
+
 ]
