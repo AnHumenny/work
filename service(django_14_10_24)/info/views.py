771ffa92494faf233916c6590cf04f5fee4cc272
Andrei Gumenniy <an.gumenniy@gmail.com>
2024-11-08 08:50:51 +0300

    Update views.py
    Fixed a bug with an incorrect last day of the current month when accessing the database

--- service(django_14_10_24)/info/views.py
+++ service(django_14_10_24)/info/views.py
@@ -10,26 +10,25 @@ from django.views.generic import (
     DetailView,
 )
 
-
 class InfoListView(ListView):
     model = Info
-    queryset = Info.objects.all().order_by("-date_created")  # Это ключевой запрос
+    queryset = Info.objects.all().order_by("-id")  
     paginate_by = 50
 
 class InfoDetailView(DetailView):
     model = Info
 
-
 def download_csv_actual_month(request):
-    now = datetime.now()
-    start_date = timezone.datetime(now.year, now.month, 1)
-    end_date = timezone.datetime(now.year, now.month, 31)
+    now = timezone.now()
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename="data.csv"'
+    first_day_of_month = now.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+    last_day_of_month = (first_day_of_month.replace(month=first_day_of_month.month + 1, day=1) - timezone.timedelta(
+        days=1)) if now.month < 12 else now.replace(year=now.year + 1, month=1, day=1) - timezone.timedelta(days=1)
     writer = csv.writer(response)
-    writer.writerow(['ID', 'Реестр', 'Дата', 'Город', 'Улица', 'Дом',
-                     'ФИО абонента', 'кабель 1', 'кабель 2', 'кабель 3', 'коннектор'])  # Замените на Ваши поля
-    for obj in Info.objects.all().filter(date_created__range=(start_date, end_date)).order_by("-date_created"):
+    writer.writerow(['ID', 'Реестр', 'Дата', 'Город', 'Улица', 'Дом', 'Квартира',
+                     'ФИО абонента', 'кабель 1', 'кабель 2', 'кабель 3', 'коннектор'])
+    for obj in Info.objects.all().filter(date_created__range=(first_day_of_month, last_day_of_month)).order_by("-id"):
         writer.writerow([obj.id, obj.reestr, obj.date_created, obj.city, obj.street, obj.home,
                          obj.apartment, obj.name, obj.cable_1, obj.cable_2, obj.cable_3, obj.connector
                          ])
@@ -45,7 +44,7 @@ def date_range_view(request):
             end_date = form.cleaned_data['end_date']
             writer = csv.writer(response)
             writer.writerow(['ID', 'Реестр', 'Дата', 'Город', 'Улица', 'Дом',
-                             'ФИО абонента', 'кабель 1', 'кабель 2', 'кабель 3', 'коннектор'])  # Замените на Ваши поля
+                             'ФИО абонента', 'кабель 1', 'кабель 2', 'кабель 3', 'коннектор'])  
             for obj in Info.objects.all().filter(date_created__range=(start_date, end_date)).order_by("-date_created"):
                 writer.writerow([obj.id, obj.reestr, obj.date_created, obj.city, obj.street, obj.home,
                                  obj.apartment, obj.name, obj.cable_1, obj.cable_2, obj.cable_3, obj.connector
