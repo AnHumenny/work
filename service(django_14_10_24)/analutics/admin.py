33ba389fd4fb44ed8a66005260b7b5e7b7006c21
Andrei Gumenniy <an.gumenniy@gmail.com>
2024-11-14 15:06:27 +0300

    Delete service(django_14_10_24)/analutics directory

--- service(django_14_10_24)/analutics/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Analutic
-
-admin.site.register(Analutic)  # Регистрируем модель.
--- service(django_14_10_24)/analutics/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class Analutic(
-    models.Model):
-    date_created = models.DateField()
-    city = models.CharField(max_length=20)
-    street = models.CharField(max_length=30)
