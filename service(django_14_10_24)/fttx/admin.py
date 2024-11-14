efc18002efe8872a6ab0ac869a1d8807ecce37bc
Andrei Gumenniy <an.gumenniy@gmail.com>
2024-11-14 15:07:34 +0300

    Delete service(django_14_10_24)/fttx directory

--- service(django_14_10_24)/fttx/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Fttx
-
-admin.site.register(Fttx)
--- service(django_14_10_24)/fttx/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FttxConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fttx'
