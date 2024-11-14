a82c6e4d1741289291a673288b4ac8c4b0436d09
Andrei Gumenniy <an.gumenniy@gmail.com>
2024-11-14 15:07:03 +0300

    Delete service(django_14_10_24)/base_station directory

--- service(django_14_10_24)/base_station/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import BaseStation
-
-admin.site.register(BaseStation)  # Регистрируем модель.
--- service(django_14_10_24)/base_station/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BaseStationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'base_station'
