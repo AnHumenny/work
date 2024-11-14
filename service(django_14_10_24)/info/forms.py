115ed3699ed08b7d71d34a45679a37f22f598e16
Andrei Gumenniy <an.gumenniy@gmail.com>
2024-11-14 15:08:44 +0300

    Delete service(django_14_10_24)/info directory

--- service(django_14_10_24)/info/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class DateForm(forms.Form):
-
-    start_date = forms.DateField(widget=forms.SelectDateWidget(), label="Начальная дата    ")
-    end_date = forms.DateField(widget=forms.SelectDateWidget(), label="Конечная дата     ")
-
