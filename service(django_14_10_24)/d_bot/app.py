4e50b623772259d22e47f42b2da8a318ae0907ae
Andrei Gumenniy <an.gumenniy@gmail.com>
2024-11-19 14:06:12 +0300

    Update and rename bot.py to app.py
    Expanded functionality for downloading and viewing downloaded photos

--- service(django_14_10_24)/d_bot/app.py
+++ service(django_14_10_24)/d_bot/app.py
@@ -7,16 +7,19 @@ import base64
 from aiogram import F
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
-from aiogram.types import Message
+from aiogram.types import Message, BufferedInputFile
 from aiogram.filters import Command, CommandObject, StateFilter
 import subprocess
 import lists
 import keyboards
 from repository import Repo
-from time import sleep
+from dotenv import load_dotenv
+
+load_dotenv()
+API_TOKEN = os.getenv('API_TOKEN')
 
 logging.basicConfig(level=logging.INFO)
-bot = Bot(token=lists.API_TOKEN)
+bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
 class SelectInfo(StatesGroup):
@@ -32,9 +35,10 @@ class Info:
 class Registred:
     admin_OK = False
     user_OK = False
+    available_user_names = []
     login = None
     name = None
-    count = -1
+    count = 0
 
 @dp.message(StateFilter(None), Command("start"))
 async def start_handler(msg: Message, state=FSMContext):
@@ -48,15 +52,15 @@ async def start_handler(msg: Message, state=FSMContext):
 # ввод и проверка пароля
 @dp.message(SelectInfo.register_user)
 async def cmd_auth(msg: Message, state: FSMContext):
-    if Registred.count > 2:
-        Registred.count = -1
+    if Registred.count > 3:
+        Registred.count = 0
     autent = msg.text.split('|')
     if len(autent) != 2:
         Registred.count += 1
         await msg.answer(
             text=f"Что то за не то с паролем :("
         )
-        if Registred.count == 2:
+        if Registred.count == 3:
             await msg.answer(
                 text="Теперь ждём минуту :("
             )
@@ -74,8 +78,7 @@ async def cmd_auth(msg: Message, state: FSMContext):
             await msg.answer(
                 text=f"Что то не получилось с паролем :("
             )
-            print("count", Registred.count)
-            if Registred.count == 2:
+            if Registred.count == 3:
                 await msg.answer(
                     text="Теперь ждём минуту :("
                 )
@@ -90,7 +93,7 @@ async def cmd_auth(msg: Message, state: FSMContext):
                     text=f"Не зашло с паролем :("
                 )
                 Registred.count += 1
-                if Registred.count == 2:
+                if Registred.count == 3:
                     await msg.answer(
                         text="Теперь ждём минуту :("
                     )
@@ -162,9 +165,11 @@ async def cmd_send_photo(
         dir_name = f"photos/fttx/{Info.city}/{Info.street}/{Info.home}/{Info.apartment}"
     if Info.form == "to":
         dir_name = f"photos/to/{Info.city}/{Info.street}/{Info.home}/{Info.apartment}"    #aparnment == entrance
+    if Info.form == "FTTX":
+        dir_name = f"photos/FTTX/{Info.city}/{Info.street}/{Info.home}/{Info.apartment}"    #aparnment == entrance
     if (any(c in r'/\:*?"<>|' for c in Info.apartment) or any(c in r'/\:*?"<>|' for c in Info.home) or
             any(c in r'/\:*?"<>|' for c in Info.street) or any(c in r'/\:*?"<>|' for c in Info.city)):
-        await message.reply("Недопустимое имя директории.")
+        await message.reply("Недопустимое имя директории!")
         return
     process = await asyncio.create_subprocess_shell(f"mkdir -p {dir_name}", stdout=subprocess.PIPE,
                                                     stderr=subprocess.PIPE)
@@ -182,28 +187,70 @@ async def cmd_send_photo(
     await Repo.insert_into_visited_date(Registred.name,
                                         f"Добавил фото в "
                                         f"{Info.form}/{Info.city}/{Info.street}/{Info.home}/{Info.apartment}")
+
 @dp.message(F.photo)
 async def view_3(msg: Message, state: FSMContext):
     if Registred.login not in lists.id_user and Registred.user_OK is False:
         await msg.answer(
             text=f"Увы. Нет доступа к внутренней информации :("
         )
+        await state.clear()
         return
-    Registred.count += 1
+    if Info.city is None:
+        await msg.reply(f"Ошибка: не указан адрес")
+        await state.clear()
+        return
+
     await bot.download(
         msg.photo[-1],
         destination=f"{os.getcwd()}/photos/{Info.form}/{Info.city}/{Info.street}/{Info.home}/"
-                    f"{Info.apartment}/{msg.photo[-1].file_id}___{Registred.count}.jpg"
+                    f"{Info.apartment}/{msg.photo[-1].file_id}+{msg.date}.jpg"
     )
-    await msg.answer(
-        "Загружено!"
-    )
-    print(f"Улица: {Info.form}\n"
-          f"Город: {Info.city}\n"
-          f"Улица: {Info.street}\n"
-          f"Дом: {Info.home}\n"
-          f"квартира(этаж для ТО): {Info.apartment}")
     await state.clear()
+    return
+
+@dp.message(Command("view"))
+async def send_photo(message: types.Message, command: CommandObject):
+    if Registred.login not in lists.log_admin and Registred.admin_OK is False:
+        await message.answer(
+            text=f"Нет доступа к внутренней информации. Требуются расширенные права"
+        )
+        return
+    if command.args is None:
+        await message.answer(
+            "Ошибка: не переданы аргументы"
+        )
+        return
+    try:
+        Info.form, Info.city, Info.street, Info.home, Info.apartment = command.args.split("/", maxsplit=4)
+        images_folder = f'photos/{Info.form}/{Info.city}/{Info.street}/{Info.home}/{Info.apartment}/'
+        if Info.city not in lists.city:
+            await message.answer(
+                f"Ошибка: неправильный формат города : {Info.city}"
+            )
+            return
+    except ValueError:
+        await message.answer(
+            "Ошибка: неправильный формат команды. Пример:\n"
+            "/send fttx/Город/улица/дом/квартира(для ТО, подьезд/подвал,чердак)"
+        )
+        return
+    await Repo.insert_into_visited_date(Registred.name,
+                                        f"Посмотрел фото в "
+                                        f"{Info.form}/{Info.city}/{Info.street}/{Info.home}/{Info.apartment}")
+    if not os.path.isdir(images_folder):
+        await message.answer(
+            "Запрашиваемая директория отсутствует.\nНабери /help"
+        )
+        return
+    images = [f for f in os.listdir(images_folder) if f.endswith(('jpg', 'jpeg', 'png', 'gif'))]
+    if images:
+        for row in images:
+            with open(os.path.join(images_folder, row), 'rb') as file:
+                 photo = BufferedInputFile(file.read(), 'uploaded_photo')
+            await message.answer_photo(photo)
+    else:
+        await message.reply("В папке нет изображений.\nНабери /help")
     return
 
 async def main():
@@ -211,3 +258,4 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
