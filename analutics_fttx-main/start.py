7890dda9c4166a5d593afa93757e736bc1a93081
Andrei Gumenniy <an.gumenniy@gmail.com>
2024-11-27 10:17:50 +0300

    Update start.py

--- analutics_fttx-main/start.py
+++ analutics_fttx-main/start.py
@@ -1,4 +1,9 @@
-import autentification
+from time import sleep
+from tkinter import messagebox
+from datetime import datetime
+from dotenv import load_dotenv
+import os
+load_dotenv()
 from mysql.connector import connect
 from tkinter import *
 import tkinter as tk
@@ -6,11 +11,11 @@ import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 matplotlib.style.use('ggplot')
+import matplotlib.pyplot as plt
 from collections import OrderedDict
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 import random
 import pandas as pd
-
 
 class Win(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -20,7 +25,7 @@ class Win(tk.Tk):
         self.date_start = tk.StringVar()
         self.date_end = tk.StringVar()
         self.street_all = tk.StringVar()
-        self.temp = ''
+        self.temp = None
         self.start_analutic()
 
     def start_analutic(self):
@@ -30,12 +35,7 @@ class Win(tk.Tk):
         frame_1.pack()
         frame_line = Frame(frame_1, relief=SUNKEN, width=1200, height=50)
         frame_line.pack(pady=5)
-        label_line = Label(frame_1,
-                           text='Для плота LINE, указываем только год,\n в строке ниже, через запятую, улицы')
-        label_line.pack(pady=5)
-        label_entry = Entry(frame_1, textvariable=self.street_all, width=66)
-        label_entry.pack(pady=5)
-        my_date_1 = tk.Label(frame_1, width=30, text='Данные по абонентам за период с ')
+        my_date_1 = tk.Label(frame_1, width=50, text='Данные по абонентам за период с YYYY-MM-DD')
         my_date_1.pack(pady=5)
         my_date_2 = tk.Entry(frame_1, width=10, textvariable=self.date_start)
         my_date_2.pack(pady=5)
@@ -51,44 +51,53 @@ class Win(tk.Tk):
         button_send_pie.pack(pady=5)
         button_send_ring = tk.Button(frame_1, text='Посмотреть RING', width=15, command=self.select_plt_ring)
         button_send_ring.pack(pady=5)
+        label_line = Label(frame_1,
+                           text='В строке, улицы через запятую')
+        label_line.pack(pady=5)
+        label_entry = Entry(frame_1, textvariable=self.street_all, width=66)
+        label_entry.pack(pady=5)
         button_send_line = tk.Button(frame_1, text='Посмотреть LINE', width=15, command=self.select_plt_line)
         button_send_line.pack(pady=5)
         but_ex = tk.Button(frame_1, width=18, text='Выход', command=self.exit_all)
         but_ex.pack(pady=5)
 
 # вертикальная столбчатая, для малого обьёма
-    def select_plt_bar(self):  
+    def select_plt_bar(self):
         temp = 'bar'
-        street_all = ''
+        street_all = None
         date_start = self.date_start.get()
         date_end = self.date_end.get()
-        self.result(temp, date_start, date_end, street_all)
+        date_line = None
+        self.result(temp, date_start, date_end,  street_all)
 
 # горизонтальная столбчатая
-    def select_plt_barh(self):  
+    def select_plt_barh(self):
         temp = 'barh'
-        street_all = ''
+        street_all = None
         date_start = self.date_start.get()
         date_end = self.date_end.get()
+        date_line = None
         self.result(temp, date_start, date_end, street_all)
 
 #кольцо проценты
     def select_plt_ring(self):
         temp = 'ring'
-        street_all = ''
+        street_all = None
         date_start = self.date_start.get()
         date_end = self.date_end.get()
+        date_line = None
         self.result(temp, date_start, date_end, street_all)
 
 #круг проценты
     def select_plt_pie(self):
         temp = 'pie'
-        street_all = ''
+        street_all = None
         date_start = self.date_start.get()
         date_end = self.date_end.get()
+        date_line = None
         self.result(temp, date_start, date_end, street_all)
 
-#детальная инфограцика по улицам, берёт из .csv
+#детальная инфографика по улицам, берёт из .csv
     def select_plt_line(self):
         temp = 'line'
         street_all = self.street_all.get()
@@ -96,22 +105,51 @@ class Win(tk.Tk):
         date_end = self.date_end.get()
         self.result(temp, date_start, date_end, street_all)
 
-#инфографика 
+    @staticmethod
+    def validate_date(date_text):
+        try:
+            datetime.strptime(date_text, '%Y-%m-%d')
+            return True
+        except ValueError:
+            return False
+
+    def create_csv(self):
+        connection = connect(host=os.getenv('host'), port=os.getenv('port'), user=os.getenv('user'),
+                               password=os.getenv('password'), database=os.getenv('database'))
+        date_start = self.date_start.get()
+        date_end = self.date_end.get()
+        current_year = date_end[:4]
+        query = """
+        SELECT TRIM(street) AS street, DATE_FORMAT(date, '%Y-%m') AS month FROM info WHERE date BETWEEN %s AND %s
+        """
+        cursor = connection.cursor()
+        cursor.execute(query, (date_start, date_end))
+        results = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        df = pd.DataFrame(results, columns=['street', 'month'])
+        count_df = df.groupby(['street', 'month']).size().unstack(fill_value=0)
+        count_df = count_df.transpose()
+        count_df.to_csv(f'{current_year}.csv')
+        sleep(1)
+        return
+
     def result(self, temp, date_start, date_end, street_all):
         result_query = dict()
         sorted_street = []
         list_query = []
         set_street = set()
         temp = temp
-        if date_start == '' or date_end == '':
+        year = self.date_end.get()[:4]
+        if date_start is None or date_end is None:
             self.start_analutic()
-
+        if not self.validate_date(date_start) or not self.validate_date(date_end):
+            messagebox.showinfo("Ошибка", "Неверный формат даты! Используйте YYYY-MM-DD.")
+            return
         else:
             try:
-                if temp == 'line':
-                    date_start = date_start[3]
-                conn = connect(user=autentification.user, password=autentification.password,
-                               host=autentification.host, database=autentification.database)
+                conn = connect(host=os.getenv('host'), port=os.getenv('port'), user=os.getenv('user'),
+                               password=os.getenv('password'), database=os.getenv('database'))
                 cursor = conn.cursor()
                 query_date = "SELECT date, street FROM info WHERE date BETWEEN %s AND %s"
                 cursor.execute(query_date, (date_start, date_end))
@@ -127,6 +165,7 @@ class Win(tk.Tk):
                     for rows in result:
                         result_query[rows] = result_query.get(rows, 0) + 1  # в словарь result_query
                     list_query = [[key, value] for key, value in result_query.items()]
+                    cursor.close()
                 conn.close()
             except OSError as err:
                 print('Error message')
@@ -142,18 +181,22 @@ class Win(tk.Tk):
             if temp == 'bar':
                 d = OrderedDict(sorted(list_query, key=lambda x: x[0]))  # вертикальная диаграмма
                 values = list(d.values())  # столбчатая диаграмма
+                plt.xlabel('Ось X', labelpad=120)  # Увеличиваем отступ для метки оси X
+                axes = plt.subplot(1, 1, 1)
+                axes.tick_params(axis='x', labelrotation=35)
                 plt.bar(range(len(d)), values, color='purple',
                         tick_label=sorted_street)
             if temp == 'barh':
                 d = OrderedDict(sorted(list_query, key=lambda x: x[0]))  # столбчатая диаграмма
                 values = list(d.values())  # столбчатая диаграмма
+                plt.ylabel('Ось Y', labelpad=120)  # Увеличиваем отступ для метки оси Y
                 plt.barh(range(len(d)), values, tick_label=sorted_street)  # горизонтальная диаграмма
             if temp == 'ring':
                 list_explode = []
                 d = OrderedDict(sorted(list_query, key=lambda x: x[0]))  # круговая диаграмма
                 values = list(d.values())
                 for row in range(len(list_query)):
-                    list_explode.append(round(random.random() / 2, 3))  # случайное расстояние между долями
+                    list_explode.append(round(random.random() / 3, 4))  # случайное расстояние между долями
                 fig = matplotlib.pyplot.figure()
                 plt.pie(values, labels=sorted_street, autopct='%1.1f%%',
                         explode=list_explode, rotatelabels=False)
@@ -162,12 +205,13 @@ class Win(tk.Tk):
                 d = OrderedDict(sorted(list_query, key=lambda x: x[0]))  # кольцевая диаграмма
                 values = list(d.values())
                 for row in range(len(list_query)):
-                    list_explode.append(round(random.random() / 2, 3))  # случайное расстояние между долями
+                    list_explode.append(round(random.random() / 3, 4))  # случайное расстояние между долями
                 fig = matplotlib.pyplot.figure()
                 plt.pie(values, labels=sorted_street, autopct='%1.1f%%',
                         explode=list_explode, rotatelabels=False, shadow=False, wedgeprops=dict(width=0.5))
             if temp == 'line':
-                df_clicks = pd.read_csv(date_start + '.csv', index_col=0)
+                self.create_csv()
+                df_clicks = pd.read_csv(year + '.csv', index_col=0)
                 street = street_all.split(', ')
                 fig = matplotlib.pyplot.figure()
                 for row in street:
@@ -187,8 +231,9 @@ class Win(tk.Tk):
             canvas.get_tk_widget().pack(fill='both', expand=True)
             canvas.draw()
             toolbarFrame = Frame(master=self)
-            toolbarFrame.pack()
             toolbar = NavigationToolbar2Tk(canvas, toolbarFrame)
+            toolbarFrame.pack()
+
 
     def exit_all(self):
         self.quit()
@@ -198,4 +243,3 @@ class Win(tk.Tk):
 if __name__ == "__main__":
     testObj = Win()
     testObj.mainloop()
-
